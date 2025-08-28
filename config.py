a9c23fa0a6423e05e0dfc1dae8b00238af7dd7ae
Arnav Verma <arnxvv@gmail.com>
2025-08-28 02:56:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
 MODEL_ID = "meta-llama/llama-3.2-1B-Instruct"
+
+# Update this with your HF Token
 HF_TOKEN = os.environ.get("HF_TOKEN", "YOUR_HF_TOKEN_HERE")
 
-TRAIN_FILE = "train.csv"
-TEST_FILE = "test.csv"
+TRAIN_FILE = "data/train.csv"
+TEST_FILE = "data/test.csv"
 OUTPUT_DIR = "./results"
 FINAL_MODEL_DIR = "./final_model"
